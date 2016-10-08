fa2fe2165cf0ff6db3c97bb22c1f149688cbcd1d
Patrick El-Hage <patrick.elhage@gmail.com>
2016-10-08 00:43:58 -0700

    Improved placeholder and styling component styles.

--- src/Editors/StyleEditor/StyleEditor.js
+++ src/Editors/StyleEditor/StyleEditor.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import styles from './index.css'
 
 const StyleEditor = (props) => {
   return (
     <div className="StyleEditor">
-      <button tabIndex="-1" onMouseDown={props.onBoldClick}>B</button>
-      <button tabIndex="-1" onMouseDown={props.onItalicClick}>I</button>
-      <button tabIndex="-1" onMouseDown={props.onUnderLineClick}>U</button>
+      <button tabIndex="-1" onMouseDown={props.onBoldClick}>⌘ B</button>
+      <button tabIndex="-1" onMouseDown={props.onItalicClick}>⌘ I</button>
+      <button tabIndex="-1" onMouseDown={props.onUnderLineClick}>⌘ U</button>
     </div>
   )
+}
+
+StyleEditor.propTypes = {
+  onBoldClick: PropTypes.func,
+  onItalicClick: PropTypes.func,
+  onUnderLineClick: PropTypes.func
 }
 
 export default StyleEditor
